82c5d8b2e38b5faa071209b5ca790c3dbf40e11b
rovin0805 <qhdrn67@naver.com>
2020-08-10 17:32:29 +0900

    Updates

---
+++ precache-manifest.b9261655b2242c72232d87552dfa709f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4dfeb2c20f6fc3417ea0fd1d586dab",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "cc644b45e1a64b91d2b0",
+    "url": "/movie_app/static/css/main.bb5532bb.chunk.css"
+  },
+  {
+    "revision": "b3e2cf7259859ddd7c70",
+    "url": "/movie_app/static/js/2.02cd0f0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.02cd0f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc644b45e1a64b91d2b0",
+    "url": "/movie_app/static/js/main.1e647b7b.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
